0812868ad168e346f3b9014b906023c2bbd4b585
Massivesoft <admin@massivesoft.net>
2015-09-15 15:42:13 -0500

    Update test.php

--- server/test.php
+++ server/test.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8" />
 <title>WebSocket Test</title>
 <script language="javascript" type="text/javascript">
-var wsUri = "ws://<?php echo file_get_contents("http://icanhazip.com"); ?>:8888/messages";
+var wsUri = "ws://<?php echo file_get_contents("http://icanhazip.com"); ?>:8888/echo";
 var output;
 function init() {
   output = document.getElementById("output");
